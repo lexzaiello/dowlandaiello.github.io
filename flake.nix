645247a12e172b898d629c3898edcd53f81c2689
Dowland Aiello <dowlandaiello@gmail.com>
2025-07-12 20:40:07 -0700

    Update flake

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    hugo-papermod = {
-      url = "github:adityatelange/hugo-papermod";
+    hugo-terminal = {
+      url = "github.com:panr/hugo-theme-terminal";
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, flake-utils, hugo-papermod }@inputs:
+  outputs = { self, nixpkgs, flake-utils, hugo-terminal }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
@@ -18,7 +18,7 @@
           buildInputs = [ pkgs.git pkgs.hugo pkgs.nodePackages.prettier ];
           buildPhase = ''
             mkdir -p themes
-            ln -s ${inputs.hugo-papermod} themes/papermod
+            ln -s ${inputs.hugo-terminal} themes/terminal
             ${pkgs.hugo}/bin/hugo --gc --minify
             ${pkgs.nodePackages.prettier}/bin/prettier -w public '!**/*.{js,css}'
           '';
@@ -28,7 +28,7 @@
           buildInputs = [ pkgs.hugo ];
           shellHook = ''
             mkdir -p themes
-            ln -s ${inputs.hugo-papermod} themes/papermod
+            ln -s ${inputs.hugo-terminal} themes/terminal
           '';
         };
       });
